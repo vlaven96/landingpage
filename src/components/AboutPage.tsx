6f5fe3493ec3caaeda773af6703fa737fdb9f37f
vlaven96 <vladimir.ventaniuc@gmail.com>
2025-03-24 20:08:18 +0200

    about page update

--- src/components/AboutPage.tsx
+++ src/components/AboutPage.tsx
@@ -47,8 +47,8 @@ const AboutPage: React.FC<AboutPageProps> = ({ language, hasVisited = false }) =
   const whoWeAreTitle = language === 'en' ? "Who We Are" : "Cine Suntem";
   
   const whoWeAreText = language === 'en'
-    ? "DeceneuLabs was founded by two developers who bring extensive experience from industry-leading organizations like Amazon, Microsoft, Google, BP, as well as agile startups such as Superside and Noom. Our background enables us to blend enterprise-level expertise with startup-driven agility to deliver solutions that truly drive results."
-    : "DeceneuLabs a fost fondată de doi dezvoltatori care aduc experiență vastă din organizații de top precum Amazon, Microsoft, Google, BP, precum și startup-uri agile ca Superside și Noom. Experiența noastră ne permite să îmbinăm expertiza de nivel enterprise cu agilitatea specifică startup-urilor pentru a oferi soluții care generează rezultate reale.";
+    ? "DeceneuLabs was founded by developers who bring extensive experience from industry-leading organizations like Amazon, Microsoft, Google, BP, as well as agile startups such as Superside and Noom. Our background enables us to blend enterprise-level expertise with startup-driven agility to deliver solutions that truly drive results."
+    : "DeceneuLabs a fost fondată de dezvoltatori care aduc experiență vastă din organizații de top precum Amazon, Microsoft, Google, BP, precum și startup-uri agile ca Superside și Noom. Experiența noastră ne permite să îmbinăm expertiza de nivel enterprise cu agilitatea specifică startup-urilor pentru a oferi soluții care generează rezultate reale.";
     
   const ourVisionTitle = language === 'en' ? "Our Vision" : "Viziunea Noastră";
   
